b44a2a301b6801b7f246551fb995e4f7ad6a8710
moein <moeintorogh@gmail.com>
2024-10-19 22:34:00 +0330

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace OGMoein
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.imageBox1 = new Cyotek.Windows.Forms.ImageBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(59, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(650, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(41, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.button1_KeyDown);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(586, 72);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(119, 34);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "پردازش1";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.button2_KeyDown);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 46);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(41, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "...";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.button3.KeyDown += new System.Windows.Forms.KeyEventHandler(this.button3_KeyDown);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(59, 48);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(650, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(475, 72);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(105, 34);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "پردازش2";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            this.button4.KeyDown += new System.Windows.Forms.KeyEventHandler(this.button4_KeyDown);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(366, 72);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(103, 33);
+            this.button5.TabIndex = 0;
+            this.button5.Text = "پردازش3";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            this.button5.KeyDown += new System.Windows.Forms.KeyEventHandler(this.button5_KeyDown);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(3, 3);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(143, 17);
+            this.radioButton1.TabIndex = 7;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "روشن پردازش 3 (Ctrl + F1)";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            this.radioButton1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.radioButton1_KeyDown);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.radioButton2);
+            this.panel1.Controls.Add(this.radioButton1);
+            this.panel1.Location = new System.Drawing.Point(29, 80);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(311, 25);
+            this.panel1.TabIndex = 8;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(162, 3);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(147, 17);
+            this.radioButton2.TabIndex = 8;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "خاموش پردازش 3 (Ctrl + F2)";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.radioButton2_KeyDown);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.imageBox1);
+            this.panel2.Controls.Add(this.panel1);
+            this.panel2.Controls.Add(this.button2);
+            this.panel2.Controls.Add(this.button5);
+            this.panel2.Controls.Add(this.button4);
+            this.panel2.Controls.Add(this.panel3);
+            this.panel2.Controls.Add(this.panel4);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(734, 520);
+            this.panel2.TabIndex = 9;
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.Location = new System.Drawing.Point(8, 112);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(710, 394);
+            this.imageBox1.TabIndex = 9;
+            this.imageBox1.Text = "imageBox1";
+            // 
+            // panel3
+            // 
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(734, 106);
+            this.panel3.TabIndex = 10;
+            // 
+            // panel4
+            // 
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel4.Location = new System.Drawing.Point(0, 113);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(734, 407);
+            this.panel4.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(734, 520);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.panel2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Panel panel2;
+        private Cyotek.Windows.Forms.ImageBox imageBox1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,376 @@
+﻿
+using System.Drawing;
+using System;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.CvEnum;
+using System.Threading.Tasks;
+using ImageProcessor.Processors;
+using YamlDotNet.Core.Events;
+using System.Threading;
+using System.Text.RegularExpressions;
+
+namespace OGMoein
+{
+    public partial class Form1 : Form
+    {
+        // مشخص کردن ناحیه‌ای که می‌خواهیم اسکرین شات بگیریم
+        private int x = 0; // مختصات x نقطه شروع
+        private int y = 0; // مختصات y نقطه شروع
+        // ایجاد یک شیء Bitmap با ابعاد مشخص شده
+        private Bitmap bmp = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, System.EventArgs e)
+        {
+            //if (!IsPlaformCompatable()) return;
+
+            //String win1 = "Test Window"; //The name of the window
+            //CvInvoke.cvNamedWindow(win1); //Create the window using the specific name
+
+            //Image<Bgr, Byte> img = new Image<Bgr, byte>(400, 200, new Bgr(255, 0, 0)); //Create an image of 400x200 of Blue color
+            //MCvFont f = new MCvFont(FONT.CV_FONT_HERSHEY_COMPLEX, 1.0, 1.0); //Create the font
+
+            //img.Draw("Hello, world", ref f, new System.Drawing.Point(10, 80), new Bgr(0, 255, 0)); //Draw "Hello, world." on the image using the specific font
+
+            //CvInvoke.cvShowImage(win1, img); //Show the image
+            //CvInvoke.cvWaitKey(0);  //Wait for the key pressing event
+            //CvInvoke.cvDestroyWindow(win1); //Destory the window
+            radioButton2.Checked = true;
+            button5.Focus();
+        }
+
+        private bool IsPlaformCompatable()
+        {
+            int clrBitness = Marshal.SizeOf(typeof(IntPtr)) * 8;
+            if (clrBitness != CvInvoke.UnmanagedCodeBitness)
+            {
+                MessageBox.Show(String.Format("Platform mismatched: CLR is {0} bit, C++ code is {1} bit."
+                   + " Please consider recompiling the executable with the same platform target as C++ code.",
+                   clrBitness, CvInvoke.UnmanagedCodeBitness));
+                return false;
+            }
+            return true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            textBox1.Text = openFileDialog1.FileName;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(textBox1.Text))
+            {
+                Image<Bgr, Byte> img = new Image<Bgr, byte>(textBox1.Text).Resize(400, 400, Emgu.CV.CvEnum.INTER.CV_INTER_LINEAR, true);
+
+                if (!IsPlaformCompatable()) return;
+                Image<Gray, Byte> gray = img.Convert<Gray, Byte>().PyrDown().PyrUp();
+
+                Gray cannyThreshold = new Gray(180);
+                Gray cannyThresholdLinking = new Gray(120);
+                Gray circleAccumulatorThreshold = new Gray(500);
+
+                CircleF[] circles = gray.HoughCircles(
+                cannyThreshold,
+                circleAccumulatorThreshold,
+                4.0, //Resolution of the accumulator used to detect centers of the circles
+                15.0, //min distance 
+                5, //min radius
+                0 //max radius
+                )[0]; //Get the circles from the first channel                
+
+                Image<Gray, Byte> cannyEdges = gray.Canny(cannyThreshold, cannyThresholdLinking);
+
+                // Create threads for each window
+                Thread thread1 = new Thread(() =>
+                {
+                    String win1 = "Test Window 1";
+                    CvInvoke.cvNamedWindow(win1);
+                    CvInvoke.cvShowImage(win1, cannyEdges);
+                    CvInvoke.cvWaitKey(0);
+                    CvInvoke.cvDestroyWindow(win1);
+                });
+
+                Thread thread2 = new Thread(() =>
+                {
+                    String win2 = "Test Window 2";  
+                    CvInvoke.cvNamedWindow(win2);
+                    CvInvoke.cvShowImage(win2, img);
+                    CvInvoke.cvWaitKey(0);
+                    CvInvoke.cvDestroyWindow(win2);
+                });
+
+                // Start the threads
+                thread1.Start();
+                thread2.Start();
+
+                // Wait for both threads to finish
+                //thread1.Join();
+                //thread2.Join();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            textBox2.Text = openFileDialog1.FileName;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Image<Bgr, Byte> img1 = new Image<Bgr, byte>(textBox1.Text);
+            Image<Bgr, Byte> img2 = new Image<Bgr, byte>(textBox2.Text);
+            if (!IsPlaformCompatable()) return;
+            Image<Gray, Byte> gray1 = img1.Convert<Gray, Byte>().PyrDown().PyrUp();
+            Image<Gray, Byte> gray2 = img2.Convert<Gray, Byte>().PyrDown().PyrUp();
+            Image<Gray, float> gray = gray1.MatchTemplate(gray2, TM_TYPE.CV_TM_SQDIFF_NORMED);
+            int rad = gray.Width / 2;
+            double[] minVal, maxVal;
+            Point[] minLoc, maxLoc;
+            gray.MinMax(out minVal, out maxVal, out minLoc, out maxLoc);
+
+            if (maxVal[0] > 0.8)
+            {
+                CvInvoke.cvCircle(img1, minLoc[0], 50, new MCvScalar(0, 0, 255), 0, LINE_TYPE.FOUR_CONNECTED, 2);
+
+                //Thread thread2 = new Thread(() =>
+                //{
+                //    String win2 = "Test Window 2";
+                //    CvInvoke.cvNamedWindow(win2);
+                //    CvInvoke.cvShowImage(win2, gray);
+                //    CvInvoke.cvWaitKey(0);
+                //    CvInvoke.cvDestroyWindow(win2);
+                //});
+
+                //Thread thread1 = new Thread(() =>
+                //{
+                 String win1 = "Test Window 1";
+                 CvInvoke.cvNamedWindow(win1);
+                 CvInvoke.cvShowImage(win1, img1);
+                 CvInvoke.cvWaitKey(0);
+                 CvInvoke.cvDestroyWindow(win1);
+                //});
+                //thread1.Start();
+                //thread2.Start();
+            }
+
+            //using (Image<Gray, float> result = gray.MatchTemplate(gray2, TM_TYPE.CV_TM_SQDIFF_NORMED))
+            //{
+            //    double[] minVal, maxVal;
+            //    Point[] minLoc, maxLoc;
+            //    result.MinMax(out minVal, out maxVal, out minLoc, out maxLoc);
+
+            //    String win2 = "Test Window 2";
+            //    CvInvoke.cvNamedWindow(win2);
+            //    CvInvoke.cvShowImage(win2, result);
+            //    CvInvoke.cvWaitKey(0);
+            //    CvInvoke.cvDestroyWindow(win2);
+
+            //    if (minVal[0] < 0.1) // آستانه را بر اساس نیاز تنظیم کنید
+            //    {
+            //        // موقعیت الگو در تصویر اصلی: minLoc
+            //        // شما می‌توانید یک مستطیل دور این ناحیه بکشید تا آن را مشخص کنید
+            //        CvInvoke.cvRectangle(img1, minLoc[0], maxLoc[0], new MCvScalar(0, 0, 255), 0, LINE_TYPE.CV_AA, 1);
+            //        //CvInvoke.Rectangle(img1, new Rectangle(minLoc[0], img2Gray.Size), new MCvScalar(0, 0, 255), 2);
+            //        CvInvoke.cvShowImage("Result", img1);
+            //        //CvInvoke.Imshow("Result", img1);
+            //        CvInvoke.cvWaitKey(0);
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("الگو پیدا نشد");
+            //    }
+            //}
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+                   
+        }
+
+        private void radioButton1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control && e.KeyCode == Keys.F1)
+            {
+                radioButton1.Checked = true;
+            }
+            else if (e.Control && e.KeyCode == Keys.F2)
+            {
+                radioButton2.Checked = true;
+            }
+        }
+
+        private void button5_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control && e.KeyCode == Keys.F1)
+            {
+                radioButton1.Checked = true;
+            }
+            else if (e.Control && e.KeyCode == Keys.F2)
+            {
+                radioButton2.Checked = true;
+            }
+        }
+
+        private void button1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control && e.KeyCode == Keys.F1)
+            {
+                radioButton1.Checked = true;
+            }
+            else if (e.Control && e.KeyCode == Keys.F2)
+            {
+                radioButton2.Checked = true;
+            }
+
+        }
+
+        private void button3_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control && e.KeyCode == Keys.F1)
+            {
+                radioButton1.Checked = true;
+            }
+            else if (e.Control && e.KeyCode == Keys.F2)
+            {
+                radioButton2.Checked = true;
+            }
+
+        }
+
+        private void button4_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control && e.KeyCode == Keys.F1)
+            {
+                radioButton1.Checked = true;
+            }
+            else if (e.Control && e.KeyCode == Keys.F2)
+            {
+                radioButton2.Checked = true;
+            }
+        }
+
+        private void radioButton2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control && e.KeyCode == Keys.F1)
+            {
+                radioButton1.Checked = true;
+            }
+            else if (e.Control && e.KeyCode == Keys.F2)
+            {
+                radioButton2.Checked = true;
+            }
+        }
+
+        private void button2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Control && e.KeyCode == Keys.F1)
+            {
+                radioButton1.Checked = true;
+            }
+            else if (e.Control && e.KeyCode == Keys.F2)
+            {
+                radioButton2.Checked = true;
+            }
+        }
+
+        //private Emgu.CV.Capture capture;
+        private Image<Bgr, Byte> frame;
+        private bool isCapturing = false;
+
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            //String win11 = "Win";
+            //CvInvoke.cvNamedWindow(win11);
+            //Image<Bgr, Byte> img1 = null;
+            //Thread thread2 = new Thread(() =>
+            //{
+            //    while (radioButton1.Checked)
+            //    {
+            //        // ایجاد یک شیء Graphics برای انجام عملیات گرافیکی
+            //        using (Graphics g = Graphics.FromImage(bmp))
+            //        {
+            //            // کپی کردن محتوای صفحه به داخل Bitmap
+            //            g.CopyFromScreen(x, y, 0, 0, bmp.Size);
+            //        }
+
+            //        // ذخیره تصویر
+            //        //bmp.Save("screenshot.png", System.Drawing.Imaging.ImageFormat.Png);
+            //        img1 = new Image<Bgr, byte>(bmp);
+
+            //    }
+            //});
+            //if (radioButton1.Checked) 
+            //{                 
+            //    thread2.Start();
+            //    CvInvoke.cvShowImage(win11, img1);
+            //}
+
+            //if (radioButton2.Checked)
+            //{
+            //    thread2.Abort();
+            //    CvInvoke.cvWaitKey(0);
+            //    CvInvoke.cvDestroyWindow(win11);
+            //}
+
+            if (radioButton1.Checked)
+
+            {
+                //capture = new Emgu.CV.Capture();
+                Application.Idle += new EventHandler(ProcessFrame);
+                isCapturing = true;
+            }
+            else
+            {
+                Application.Idle -= ProcessFrame;
+                //capture.Dispose();
+                isCapturing = false;
+            }
+        }
+        private void ProcessFrame(object sender, EventArgs e)
+        {
+            if (isCapturing)
+            {
+                using (Graphics g = Graphics.FromImage(bmp))
+                {
+                    // کپی کردن محتوای صفحه به داخل Bitmap
+                    g.CopyFromScreen(x, y, 0, 0, bmp.Size);
+                }
+
+                // ذخیره تصویر
+                //bmp.Save("screenshot.png", System.Drawing.Imaging.ImageFormat.Png);
+                frame = new Image<Bgr, byte>(bmp).Resize(imageBox1.Size.Width, imageBox1.Size.Height, Emgu.CV.CvEnum.INTER.CV_INTER_LINEAR, true);
+                //frame = capture.QueryFrame();
+
+                if (!String.IsNullOrEmpty(textBox2.Text))
+                {
+                    Image<Bgr, Byte> img2 = new Image<Bgr, byte>(textBox2.Text);
+                    if (!IsPlaformCompatable()) return;
+                    Image<Gray, Byte> gray1 = frame.Convert<Gray, Byte>().PyrDown().PyrUp();
+                    Image<Gray, Byte> gray2 = img2.Convert<Gray, Byte>().PyrDown().PyrUp();
+                    Image<Gray, float> gray = gray1.MatchTemplate(gray2, TM_TYPE.CV_TM_SQDIFF_NORMED);
+                    int rad = gray.Width / 2;
+                    double[] minVal, maxVal;
+                    Point[] minLoc, maxLoc;
+                    gray.MinMax(out minVal, out maxVal, out minLoc, out maxLoc);
+
+                    if (maxVal[0] > 0.8)
+                    {
+                        CvInvoke.cvCircle(frame, minLoc[0], 50, new MCvScalar(0, 0, 255), 0, LINE_TYPE.FOUR_CONNECTED, 2);
+                    }
+                }
+
+                imageBox1.Image = frame.Bitmap;
+            }
+        }
+    }
+}
